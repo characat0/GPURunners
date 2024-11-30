d7c41b43b1d4d1ce1755ae586a2fa82e83f266ff
characat0 <36453977+characat0@users.noreply.github.com>
2024-11-30 11:25:20 -0500

    new entrypoint

--- vastai/entrypoint.sh
+++ vastai/entrypoint.sh
@@ -1,13 +1,20 @@
 #! /bin/bash
 
-CONTAINER_LABEL=$(cat ~/.vast_containerlabel)
-INSTANCE_ID=${CONTAINER_LABEL#"C."}
+pip install vastai
+trap 'vastai destroy instance $CONTAINER_ID' EXIT
 
 EXTRA_LABELS="vastai"
 
-if [ -n "$INSTANCE_ID" ]; then
-  EXTRA_LABELS="${EXTRA_LABELS},${INSTANCE_ID}"
+if [ -z "$CONTAINER_ID" ]; then
+  echo "CONTAINER_ID not defined"
+fi
+if [ ! -f ~/.vast_api_key ]; then
+  echo "~/.vast_api_key not found, regenerating"
+  cat ~/.ssh/authorized_keys | md5sum | awk '{print $1}' > ssh_key_hv; echo -n $VAST_CONTAINERLABEL | md5sum | awk '{print $1}' > instance_id_hv; head -c -1 -q ssh_key_hv instance_id_hv > ~/.vast_api_key;
+fi
+
+if [ -n "$CONTAINER_ID" ]; then
+  EXTRA_LABELS="${EXTRA_LABELS},${CONTAINER_ID}"
 fi
 
 bash ./register.sh
-
